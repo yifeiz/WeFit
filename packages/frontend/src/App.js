daa7302875e381f6fc0aa4dde3e8c6e15394e1a6
mzhang <muen2001@hotmail.com>
2020-11-21 11:23:59 -0500

    .

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AIYAAAAAHHH
         </p>
         <a
           className="App-link"
