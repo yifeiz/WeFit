c43ec4c04d2297f6644b5f8bc693188a575efeee
Yifei Zhang <yifei7.zhang@gmail.com>
2020-11-21 13:26:56 -0500

    init playing around w/ poseNet

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,28 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Webcam from './Webcam';
-import reportWebVitals from './reportWebVitals';
-const videoConstraints = {
-  width: { min: 480 },
-  height: { min: 720 },
-  aspectRatio: 0.6666666667
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <Webcam 
-    videoConstraints={videoConstraints} 
-    width={480} 
-    height={720}
-   />,
-  document.getElementById("webcam")
-);
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
