ee4e20efb7f9bf048758264c3e921b81b923a67d
mzhang <muen2001@hotmail.com>
2020-11-21 11:20:15 -0500

    added webcam

---
+++ packages/frontend/src/Webcam.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Webcam from "react-webcam";
+ 
+const WebcamComponent = () => <Webcam />;
+
+export default WebcamComponent;
--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Webcam from './Webcam';
 import reportWebVitals from './reportWebVitals';
+const videoConstraints = {
+  width: { min: 480 },
+  height: { min: 720 },
+  aspectRatio: 0.6666666667
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Webcam 
+    videoConstraints={videoConstraints} 
+    width={480} 
+    height={720}
+   />,
+  document.getElementById("root")
 );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
